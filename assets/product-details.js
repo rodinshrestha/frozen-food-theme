bfb28e4d8d2989a86c20826f279a7bf9f90354af
rodinshrestha <rodinshrestha@gmail.com>
2025-07-24 12:39:45 +0545

    photo gallery added

--- assets/product-details.js
+++ assets/product-details.js
@@ -223,10 +223,6 @@ document.addEventListener("DOMContentLoaded", () => {
         //   delay: 10000,
         //   disableOnInteraction: true,
         // },
-        // navigation: {
-        //   nextEl: "#promotional-banner-next",
-        //   prevEl: "#promotional-banner-prev",
-        // },
         slidesPerView: 1,
         spaceBetween: 0,
         effect: "slide", // Or "fade"
